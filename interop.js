31739d541066e67d3fcbac8e615e89517f6cabca
naganthanganeshbabu <naganathanganeshbabu@syncfusion.com>
2020-09-30 21:55:51 +0530

    Min intervale mouse moving files

---
+++ interop.js
@@ -0,0 +1,20 @@
+﻿
+
+
+function onMouseDown(elem, component, interval) {
+    elem.addEventListener('mousedown', _.throttle(e => {
+        component.invokeMethodAsync('HandleMouseDown', e.offsetX, e.offsetY);
+    }, interval));
+};
+
+function onMouseMove(elem, component, interval) {
+    elem.addEventListener('mousemove', _.throttle(e => {
+        component.invokeMethodAsync('HandleMouseMove', e.clientX, e.clientY);
+    }, interval));
+};
+
+function onMouseUp(elem, component, interval) {
+    elem.addEventListener('mouseup', _.throttle(e => {
+        component.invokeMethodAsync('HandleMouseUp', e.offsetX, e.offsetY);
+    }, interval));
+};
